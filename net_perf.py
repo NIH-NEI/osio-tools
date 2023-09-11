8a41ec881dfbd3ff57db41a51a0c87ef6ca7a2a2
Corder <cordersm@neinsc68b.nih.gov>
2023-09-11 16:21:45 -0400

    Added bash and python scripts

---
+++ net_perf.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import sys
+import subprocess
+
+RED = '\033[0;31m'
+GREEN = '\033[0;32m'
+BOLD = '\033[1m'
+RESET = '\033[0m'
+
+if len(sys.argv) < 3:
+    print("Usage: python3 net_perf.py <target> <wallplate>")
+    print("<target> = Server IP Address or Hostname")
+    print("<wallplate> = Wallplate ID Number")
+    exit(1)
+
+TARGET = sys.argv[1]
+WALLPLATE = sys.argv[2]
+
+flags = [
+    '',
+    '-P 2',
+    '-P 7',
+    '-R',
+    '-R -P 2',
+    '-R -P 7',
+    '-i 0.5 -O 2',
+    '-i 0.5 -O 2 -P 7',
+    '-u -b 0',
+    '-u -b 0 -P 2',
+    '-u -b 0 -P 7'
+]
+
+IPERF = f'iperf3 -c {TARGET} -V'
+MESSAGE = "Running iperf3 from client to server"
+OUTPUT = f'bioteam_{WALLPLATE}-{TARGET}.txt'
+
+def run_command(command):
+    subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+def main():
+    run_command(f'ping -c 10 {TARGET} >> {OUTPUT}')
+    run_command(f'traceroute {TARGET} &> /dev/null >> {OUTPUT}')
+
+    for flag in flags:
+        run_command(f'{IPERF} {flag} >> {OUTPUT}')
+
+if __name__ == '__main__':
+    main()
---
+++ net_perf.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BOLD='\033[1m'
+RESET='\033[0m'
+
+if [ $# -lt 2 ]; then
+  echo "Usage: ./net_perf.sh <target> <wallplate>"
+  echo "<target> = Server IP Address or Hostname"
+  echo "<wallplate> = Wallplate ID Number"
+  exit 1
+fi
+
+TARGET=$1
+WALLPLATE=$2
+
+declare -a flags
+
+# Disable individual iperf3 tests by commenting out flags below
+   flags[0]=''
+   flags[1]='-P 2'
+   flags[2]='-P 7'
+   flags[3]='-R'
+   flags[4]='-R -P 2'
+   flags[5]='-R -P 7'
+   flags[6]='-i 0.5 -O 2'
+   flags[7]='-i 0.5 -O 2 -P 7'
+   flags[8]='-u -b 0'
+   flags[9]='-u -b 0 -P 2'
+   flags[10]='-u -b 0 -P 7'
+
+IPERF="iperf3 -c $TARGET -V"
+MESSAGE="Running iperf3 from client to server"
+OUTPUT=bioteam_$WALLPLATE-$TARGET.txt
+
+echo -ne "${RED}${BOLD} ...Running ping test\n${RESET}" && ping -c 10 $TARGET >> $OUTPUT
+  
+echo -ne "${RED}${BOLD} ...Running traceroute test\n${RESET}" && traceroute $TARGET &> /dev/null >> $OUTPUT
+   
+for flag in "${flags[@]}"; do
+	echo -ne "${RED}${BOLD} ...Running iperf3 with $flag\n${RESET}" && $IPERF $flag >> $OUTPUT
+done
