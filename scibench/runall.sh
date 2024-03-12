7cd6a09a86e18ca74f67c468a7e87c458c6c79a8
Nick George <nicholas@bioteam.net>
2024-03-12 12:41:52 -0400

    add serial number to results directory

--- scibench/runall.sh
+++ scibench/runall.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 set -e
-
-RESULTS="benchmark-results"
+SERIAL=$(system_profiler SPHardwareDataType | awk '/Serial/ {print $4}')
+RESULTS="benchmark-results-${SERIAL}"
 rm -rf $RESULTS
 rm -rf venv
 mkdir -p $RESULTS
