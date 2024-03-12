802ec732fbc27e0138a0fd6f15e2e4a91de1b53f
Nick George <nicholas@bioteam.net>
2024-03-12 12:26:59 -0400

    fix hyperfine command

--- scibench/runall.sh
+++ scibench/runall.sh
@@ -15,11 +15,17 @@ python3 -m pip install -U scikit-learn 'scikit-image[data]' matplotlib numpy sci
 
 echo "[INFO] running python image benchmarks"
 IMRESULTS="${RESULTS}/python-image-benchmarks.json"
-hyperfine "python3 ${BENCH_PATH}/image-bench.py --runs 10 --warmup 2 --export-json ${IMRESULTS}"
+hyperfine "python3 ${BENCH_PATH}/image-bench.py"\
+  --runs 10 \
+  --warmup 2 \
+  --export-json "${IMRESULTS}"
 
 echo "[INFO] running python scikit learn benchmarks"
 MLRESULTS="${RESULTS}/ml-benchmarks.json"
-hyperfine "python3 ${BENCH_PATH}/ml-bench.py --runs 10 --warmup 2 --export-json ${MLRESULTS}"
+hyperfine "python3 ${BENCH_PATH}/ml-bench.py"\
+  --runs 10 \
+  --warmup 2 \
+  --export-json "${MLRESULTS}"
 
 echo "[INFO] Python benchmarks done."
 echo "[INFO] Running sysbench benchmarks"
