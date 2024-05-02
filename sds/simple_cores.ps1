09e0c2c1b06b5d989e4b72f68892518b497125cb
Alex Oumantsev <oumantsevaa@nih.gov>
2024-05-02 13:52:15 -0400

    fix syntax and debug output added

--- sds/simple_cores.ps1
+++ sds/simple_cores.ps1
@@ -1,17 +1,11 @@
 
-$storage = "\\neivast.nei.nih.gov\data\osio_admin\"
+$storage = "\\neivast.nei.nih.gov\data\osio_admin\test"
 
 
 param(
   [Parameter(Mandatory=$true)]
   [String]$sds_folder
 )
-
-if ($sds_folder -eq "") {
-  Write-Host "No folder specified, exiting"
-  Exit
-  }
-
 
 
 $cores = @()
@@ -20,72 +14,77 @@ $cores +=
 [PSCustomObject]@{
   Path = 'biological_imaging_core'
   User = @("farissr")
-}]
+}
 
 $cores +=
 [PSCustomObject]@{
   Path = 'biological_imaging_core\confocal_microscopy'
   User = @("farissr","whitend")
-}]
+}
 
 $cores +=
 [PSCustomObject]@{
   Path = 'biological_imaging_core\histology'
   User = @("camposm")
-}]
+}
 
 $cores +=
 [PSCustomObject]@{
   Path = 'biological_imaging_core\structural_biology'
   User = @("sagarv")
-}]
+}
 
 $cores +=
 [PSCustomObject]@{
   Path = 'biological_imaging_core\transmission_electron_microscopy'
   User = @("bautistawd")
-}]
+}
 
 $cores +=
 [PSCustomObject]@{
   Path = 'flow_cytometry_core'
   User = @("villasmilr")
-}]
+}
 
 $cores +=
 [PSCustomObject]@{
   Path = 'genetic_engineering_core'
   User = @("dongl")
-}]
+}
 
 $cores +=
 [PSCustomObject]@{
   Path = 'ocular_gene_therapy_core'
   User = @("lit4")
-}]
+}
 
 $cores +=
 [PSCustomObject]@{
   Path = 'visual_function_core'
   User = @("qianh")
-}]
+}
 
 
 
 
 $corespath=$storage+$sds_folder+"\cores"
-
+Write-Host "DEBUG: corespath $corespath"
 If(!(test-path $corespath))
 {
+  Write-Host "DEBUG: Creating $corespath"
+
   New-Item -ItemType Directory -Path $corespath
 }
-  
+ 
 $CACL = Get-Acl -Path $corespath
 
 
 foreach ($core in $cores) {
   $corepath=$corespath+"\"+$core.Path
+  Write-Host "DEBUG: corepath $corepath"
+
   If(!(test-path $corepath)) {
+    Write-Host "DEBUG: Creating $corepath"
     New-Item -ItemType Directory -Path $corepath
   }
   $ACL = Get-Acl -Path $corepath
