f3ffa23f8507321f380966ca02b16af9c873d6fd
aaOumantsev <aaoumantsevaa@nih.gov>
2024-06-05 09:30:18 -0400

    Initial commit of the globus_google drive script debug

---
+++ sds/globus_google_drive.ps1
@@ -0,0 +1,55 @@
+param(
+    [string]$gdrive,
+    [string]$csvFile
+)
+
+
+$sds = "\\neivast.nei.nih.gov\data"
+
+If(!(test-path $storage\$sds_folder))
+{
+    Write-Host "Error: $storage\$sds_folder does not exist"
+    Exit
+}
+
+# Read the CSV file
+$csvData = Import-Csv -Path $csvFile | select source_path, destination_path
+#$csvData = $csvDataRaw | ForEach-Object {$_ -replace "/","\"}
+
+# Sort the data based on the second column
+$sortedData = $csvData | Sort-Object source_path
+
+# Group by unique value for source_path
+$uniqueValues = $sortedData | Group-Object source_path
+
+write-host "DEBUG: Unique Loop"
+# Loop through each unique value
+foreach ($value in $uniqueValues) {
+    # Get the number of repetitions for the current value
+    $repetitionsCount = $value.Count
+    $src = join-path -Path "$gdrive" -ChildPath "$($value.Group.source_path[0])"
+    $dst = join-path -Path "$sds" -ChildPath "$($value.Group.destination_path[0])"
+
+    Write-Host "DEBUG: count: $($value.Count)  source: $($value.Group.source_path[0]) dest: $($value.Group.destination_path[0])"
+    Write-Host "DEBUG: copy $src $dst"
+    #Copy-Item "$src" "$dst"
+    If(!(test-path "$dst"))
+    {
+        #Write-Host "Error: $dst does not exist"
+        #Exit
+    }
+
+    # Perform a loop for the number of repetitions
+    for ($i = 1; $i -lt $value.Count; $i++) {
+        $srci = $src+"($i)"
+        $dsti = $dst+"($i)"
+        Write-Host "DEBUG: copy $srci $dsti"
+        #Copy-Item "$srci" "$dsti"
+        If(!(test-path "$dsti"))
+        {
+            #Write-Host "Error: $dsti does not exist"
+            #Exit
+        }
+    }
+}
+
