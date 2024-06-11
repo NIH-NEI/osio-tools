821799cfee97f300dcf40bd6b6af2a7667e6ce93
aaOumantsev <aaoumantsevaa@nih.gov>
2024-06-11 14:15:47 -0400

    Created rename function and test for existing files

--- sds/globus_google_drive.ps1
+++ sds/globus_google_drive.ps1
@@ -4,17 +4,26 @@ param(
 )
 
 
+function grename ($path, $i) {
+    $basename = [IO.Path]::GetFileNameWithoutExtension($path)
+    $extension = [IO.Path]::GetExtension($path)
+    $fdir = [IO.Path]::GetDirectoryName($path)
+    $newname = "$fdir\$basename ($i)$extension"
+    return $newname
+}
+
+
 $sds = "\\neivast.nei.nih.gov\data\osio_admin\test"
 
-If(!(test-path $storage\$sds_folder))
+If(!(test-path $sds))
 {
-    Write-Host "Error: $storage\$sds_folder does not exist"
+    Write-Host "Error: $sds does not exist"
     Exit
 }
 
 # Read the CSV file
 $csvData = Import-Csv -Path $csvFile | select source_path, destination_path
-#$csvData = $csvDataRaw | ForEach-Object {$_ -replace "/","\"}
+
 
 # Sort the data based on the second column
 $sortedData = $csvData | Sort-Object source_path
@@ -26,9 +35,17 @@ write-host "DEBUG: Unique Loop"
 # Loop through each unique value
 foreach ($value in $uniqueValues) {
     # Get the number of repetitions for the current value
-    $repetitionsCount = $value.Count
-    $src = join-path -Path "$gdrive" -ChildPath "$($value.Group.source_path[0])"
-    $dst = join-path -Path "$sds" -ChildPath "$($value.Group.destination_path[0])"
+    #$repetitionsCount = $value.Count
+    if ($value.count -eq 1)
+    {
+        $src = join-path -Path "$gdrive" -ChildPath "$($value.Group.source_path)"
+        $dst = join-path -Path "$sds" -ChildPath "$($value.Group.destination_path)"
+    }
+    else
+    {
+        $src = join-path -Path "$gdrive" -ChildPath "$($value.Group.source_path[0])"
+        $dst = join-path -Path "$sds" -ChildPath "$($value.Group.destination_path[0])"
+    }
 
     Write-Host "DEBUG: count: $($value.Count)  source: $($value.Group.source_path[0]) dest: $($value.Group.destination_path[0])"
     
@@ -43,19 +60,32 @@ foreach ($value in $uniqueValues) {
     {
         New-Item -Path (Split-Path -Path  "$dst")
     }
+    # if destination file already exists on SDS (maybe previous copy operation)
+    if (test-path "$dst")
+    {
+        Write-Host "Warning destination file $dst already exists, type y to overwrite"
+        $choice = Read-Host
+
+        if ($choice -ne "y") 
+        {
+            Write-host "Will not overwrite, please manually clean up destination file and re-run this script"
+            Exit
+        }
+
+    }
     
     Write-Host "DEBUG: copy $src $dst"
     Copy-Item "$src" "$dst"
     If(!(test-path "$dst"))
     {
-        Write-Host "Error: Destination $dst does not exist"
+        Write-Host "Error: Copy Failed - Destination $dst does not exist"
         Exit
     }
 
     # Perform a loop for the number of repetitions
     for ($i = 1; $i -lt $value.Count; $i++) {
-        $srci = $src+"($i)"
-        $dsti = $dst+"($i)"
+        $srci = grename $src $i
+        $dsti = grename $dst $i
 
         If(!(test-path "$srci"))
         {
@@ -64,11 +94,11 @@ foreach ($value in $uniqueValues) {
         }
 
         Write-Host "DEBUG: copy $srci $dsti"
-        #Copy-Item "$srci" "$dsti"
+        Copy-Item "$srci" "$dsti"
         If(!(test-path "$dsti"))
         {
-            #Write-Host "Error: $dsti does not exist"
-            #Exit
+            Write-Host "Error: Copy Failed - Destination $dsti does not exist"
+            Exit
         }
     }
 }
