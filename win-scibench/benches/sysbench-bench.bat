1af4a91dfe803499826db7bd93ebcd1848610ef2
picklem7 <32364088+picklem7@users.noreply.github.com>
2024-12-17 13:43:56 -0500

    Update sysbench-bench.bat
    Added warmup runs to match Mac benchmarks.

--- win-scibench/benches/sysbench-bench.bat
+++ win-scibench/benches/sysbench-bench.bat
@@ -10,6 +10,6 @@ SET _INTERPOLATION_1=
 FOR /f "delims=" %%a in ('sysctl -n hw.ncpu') DO (SET "_INTERPOLATION_1=!_INTERPOLATION_1! %%a")
 SET "!_INTERPOLATION_1:~1!HALF_CORES=$(($CORES / 2))"
 mkdir "-p" "!OUTDIR!"
-hyperfine "sysbench cpu run --threads=!HALF_CORES!" "--runs" "10" "--export-json" "!OUTDIR!/sysbench-cpu.json"
-hyperfine "sysbench memory run --threads=!HALF_CORES!" "--runs" "10" "--export-json" "!OUTDIR!/sysbench-mem.json"
-hyperfine "sysbench mutex run --threads=!HALF_CORES!" "--runs" "10" "--export-json" "!OUTDIR!/sysbench-mem.json"
+hyperfine "sysbench cpu run --threads=!HALF_CORES!" "--runs" "10" "--warmup" "2" "--export-json" "!OUTDIR!/sysbench-cpu.json"
+hyperfine "sysbench memory run --threads=!HALF_CORES!" "--runs" "10" "--warmup" "2" "--export-json" "!OUTDIR!/sysbench-mem.json"
+hyperfine "sysbench mutex run --threads=!HALF_CORES!" "--runs" "10" "--warmup" "2" "--export-json" "!OUTDIR!/sysbench-mem.json"
